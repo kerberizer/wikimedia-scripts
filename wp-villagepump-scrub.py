dde1b9cef446c17e397f470cf61917720fb04078
Luchesar V. ILIEV <luchesar.iliev@gmail.com>
2016-10-07 11:13:31 +0300

    Release the scripts under CC0 license

--- wp-villagepump-scrub.py
+++ wp-villagepump-scrub.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8  -*-
 
+# wikimedia-scripts - scripts driving the Kerberizer bot on Wikimedia
+#
+# Written in 2011-2016 by Luchesar V. ILIEV <luchesar.iliev@gmail.com>
+#
+# To the extent possible under law, the author(s) have dedicated all
+# copyright and related and neighboring rights to this software to the
+# public domain worldwide. This software is distributed without any
+# warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication
+# along with this software. If not, see
+# <http://creativecommons.org/publicdomain/zero/1.0/>.
+
 import pywikibot
 
 villagePumpTitle = u'Уикипедия:Разговори'
