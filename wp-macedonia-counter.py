58cb2bb4b13c2700f97bdef8bc6f283d96bd88de
Luchesar V. ILIEV <luchesar.iliev@gmail.com>
2021-06-13 19:04:21 +0300

    MK: Update to the actual code that runs
    It has been running this way for maybe years already, but for some reasons hasn't been committed.

--- wp-macedonia-counter.py
+++ wp-macedonia-counter.py
@@ -2,7 +2,7 @@
 
 # wikimedia-scripts - scripts driving the Kerberizer bot on Wikimedia
 #
-# Written in 2011-2017 by Luchesar V. ILIEV <luchesar.iliev@gmail.com>
+# Written in 2011-2021 by Luchesar ILIEV <luchesar.iliev@gmail.com>
 #
 # To the extent possible under law, the author(s) have dedicated all
 # copyright and related and neighboring rights to this software to the
@@ -13,69 +13,22 @@
 # along with this software. If not, see
 # <http://creativecommons.org/publicdomain/zero/1.0/>.
 
-import os.path, pickle, locale, datetime, pywikibot
-from pywikibot import pagegenerators
 
-fname_pageTitlesLast = os.path.expanduser('~/.wp-macedonia-counter/titles-last.dat')
-fname_lastDateTime = os.path.expanduser('~/.wp-macedonia-counter/datetime-last.dat')
+from datetime import datetime as dt
+import locale
 
-f = open(fname_pageTitlesLast, 'rb')
-pageTitlesLast = pickle.load(f)
-f.close()
+import pywikibot as pwb
 
-f = open(fname_lastDateTime, 'rb')
-lastDateTime = pickle.load(f)
-f.close()
 
-mySite = pywikibot.Site()
-generatorKey = 'Категория:Портал:Македония/Тематични статии'
-countPage = pywikibot.Page(mySite, 'Уикипедия:Македония/Брояч')
-datePage = pywikibot.Page(mySite, 'Уикипедия:Македония/Брояч/Дата')
-diffPage = pywikibot.Page(mySite, 'Уикипедия:Македония/Брояч/Промени')
-
+w = pwb.Site()
+cat_mk = pwb.Category(w, 'Категория:Портал:Македония/Тематични статии')
+page_counter = pwb.Page(w, 'Уикипедия:Македония/Брояч')
+page_date = pwb.Page(w, 'Уикипедия:Македония/Брояч/Дата')
 locale.setlocale(locale.LC_TIME, 'bg_BG.UTF-8')
-currentDateTime = datetime.datetime.now().strftime('%H:%M на %e %B %Y').lower()
+page_date.text = dt.now().strftime('%e %B %Y').lower()
+page_date.save('Бот: актуализация на датата')
 locale.resetlocale(locale.LC_TIME)
-
-referringPages = pagegenerators.CategorizedPageGenerator(pywikibot.Category(mySite, generatorKey), recurse = True, namespaces = [0])
-
-pageTitles=set()
-count = 0
-for page in referringPages:
-    count += 1
-    pageTitles.add(page.title())
-
-addedTitles = pageTitles.difference(pageTitlesLast)
-removedTitles = pageTitlesLast.difference(pageTitles)
-
-if addedTitles or removedTitles:
-
-    f = open(fname_lastDateTime, 'wb')
-    pickle.dump(currentDateTime, f)
-    f.close()
-    f = open(fname_pageTitlesLast, 'wb')
-    pickle.dump(pageTitles, f)
-    f.close()
-
-    listDiff = '__NOEDITSECTION__\n'
-    listDiff += '{|\n|<code>' + lastDateTime + '</code>\n|-\n'
-    listDiff += '| align="center" | <big>\'\'\'↓\'\'\'</big>\n'
-    listDiff += '|-\n|<code>' + currentDateTime + '</code>\n|}\n'
-    listDiff += '\n== Добавени ==\n'
-
-    for article in addedTitles:
-        listDiff += '* {{статия|' + article + '}}\n'
-
-    listDiff += '\n== Премахнати ==\n'
-
-    for article in removedTitles:
-        listDiff += '* {{статия|' + article + '}}\n'
-
-    countPage.text = str(count)
-    datePage.text = currentDateTime
-    diffPage.text = listDiff
-    countPage.save('Бот: актуализация на брояча')
-    datePage.save('Бот: актуализация на датата')
-    diffPage.save('Бот: актуализация на промените')
+page_counter.text = cat_mk.categoryinfo['pages']
+page_counter.save('Бот: актуализация на брояча')
 
 # vim:set ts=4 sts=4 sw=4 et:
